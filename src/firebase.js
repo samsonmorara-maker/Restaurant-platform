d9a2991c93c4a4287f5dc0db48c3b5136ac85f69
samsonmorara-maker/samsongit config  --global user.email samson.morara@student.moringaschool.com <samson.morara@student.moringaschool.com>
2026-05-17 23:13:06 +0300

    workflow added for deployment

--- src/firebase.js
+++ src/firebase.js
@@ -4,23 +4,15 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth"
 
 
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA1BNGmVjC-DDFB8kX_yLivVtQQtRAVUCw",
-  authDomain: "restaurants-platform-8159f.firebaseapp.com",
-  projectId: "restaurants-platform-8159f",
-  storageBucket: "restaurants-platform-8159f.firebasestorage.app",
-  messagingSenderId: "141343947834",
-  appId: "1:141343947834:web:96196174ee5e576d6d4903",
-  measurementId: "G-MZ20HSBTC2"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app)
